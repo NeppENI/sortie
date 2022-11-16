c87300c78fb48fc81816a7038df891730ecd9e2e
ty-recz <rethi.85@hotmail.fr>
2022-11-16 08:50:45 +0100

    avancement Thierry, bugg block body à corriger

---
+++ src/Controller/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+
+{
+
+    #[Route ("/" , name:"main_home")]
+    public function home()
+    {
+        return $this->render('home.html.twig');
+    }
+
+    #[Route ("/register" , name:"main_register")]
+    public function register()
+    {
+        return $this->render('register.html.twig');
+    }
+
+    #[Route ("/login" , name:"main_login")]
+    public function login(): \Symfony\Component\HttpFoundation\Response
+    {
+        return $this->render('login.html.twig');
+    }
+
+}
