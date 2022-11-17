e5df700d31dc0919702d9202da7ce550ccce9deb
ty-recz <rethi.85@hotmail.fr>
2022-11-17 15:09:00 +0100

    Back Thierry

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,17 +15,6 @@ class MainController extends AbstractController
         return $this->render('home.html.twig');
     }
 
-    #[Route ("/register" , name:"main_register")]
-    public function register()
-    {
-        return $this->render('register.html.twig');
-    }
-
-    #[Route ("/login" , name:"main_login")]
-    public function login(): \Symfony\Component\HttpFoundation\Response
-    {
-        return $this->render('login.html.twig');
-    }
 
     #[Route ("/profil" , name:"main_profil")]
     public function profil(): \Symfony\Component\HttpFoundation\Response
