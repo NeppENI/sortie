c9a7f8c0a13e805c2b8a7df4d891a84fbc82bcd0
tmion <theo.mion2022@campus-eni.fr>
2022-11-18 11:41:56 +0100

    Théo - page d'accueil fonctionnelle - Vendredi 10:58

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,20 +9,22 @@ class MainController extends AbstractController
 
 {
 
-    #[Route ("/" , name:"main_home")]
+    #[Route ("/", name: "main_home")]
     public function home()
     {
         return $this->render('main/home.html.twig');
     }
 
-    #[Route ("/profil" , name:"main_profil")]
+    #[Route ("/profil", name: "main_profil")]
     public function profil(): \Symfony\Component\HttpFoundation\Response
     {
         return $this->render('main/profil.html.twig');
     }
 
-    #[Route ("/admin/campus" , name:"admin_campus")]
+    #[Route ("/admin/campus", name: "admin_campus")]
     public function campus(): \Symfony\Component\HttpFoundation\Response
     {
         return $this->render('admin/campus.html.twig');
+    }
+
 }
